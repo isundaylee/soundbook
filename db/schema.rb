f77d4d8a18430956e52fc78ab3437fc41c90f88b
Jiahao Li <isundaylee.reg@gmail.com>
2014-03-23 17:56:36 -0400

    Adds description to songs.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140323214006) do
+ActiveRecord::Schema.define(version: 20140323215249) do
 
   create_table "albums", force: true do |t|
     t.string   "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140323214006) do
     t.integer  "album_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   add_index "songs", ["album_id"], name: "index_songs_on_album_id"
