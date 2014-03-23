adfe0b49002bcf5a51bc7635565f21034f061065
Jiahao Li <isundaylee.reg@gmail.com>
2014-03-23 16:40:03 -0400

    Initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Soundbook::Application.config.secret_key_base = 'bf16fd74494542f6aad76d93a85e57be71119eeea7458aad981ce23c1efc045e6a88a3df8cfb5dc2c9c7bc4c42bd23bd2032d1ab7481cc41951f5806da4f3e0c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Soundbook::Application.config.session_store :cookie_store, key: '_soundbook_session'
