605db2601087b3dfecb15016884b0eae18f6ca15
Jiahao Li <isundaylee.reg@gmail.com>
2014-07-21 02:25:37 +0800

    Adds 'reveal' param to index player for direct plays.

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,5 +1,12 @@
 class PlayerController < ApplicationController
   def index
+    song_id = params[:reveal]
+
+    if song_id
+      @song = Song.find(song_id) rescue nil
+      @album = @song.album if @song
+    end
+
     render layout: false
   end
 end
