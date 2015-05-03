d30844531846d8b04b2e8b16347e0946517a2bb0
Jiahao Li <isundaylee.reg@gmail.com>
2015-05-03 17:57:24 -0400

    Apparently this is actually necessary.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,7 @@ class Song < ActiveRecord::Base
 
   has_attached_file :song
   # validates_attachment_content_type :song, content_type: ['audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio']
+  do_not_validate_attachment_file_type :song
 
   def song_url
     if Settings.is_mirror
