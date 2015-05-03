652f3c2ea26aab8b84bc2d72f5fdafcf0ea62f6e
Jiahao Li <isundaylee.reg@gmail.com>
2015-05-03 18:00:51 -0400

    Hope this actually fixes it.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,7 @@ class Song < ActiveRecord::Base
   belongs_to :album
 
   has_attached_file :song
-  # validates_attachment_content_type :song, content_type: ['audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio']
-  do_not_validate_attachment_file_type :song
+  validates_attachment_content_type :song, content_type: ['audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio']
 
   def song_url
     if Settings.is_mirror
---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,1 @@
+Paperclip.options[:content_type_mappings] = {mp3: 'audio/mp3'}
